102f5cb1cd3388f54058b2d21645626cda954bfd
seanking8 <g00367593@gmit.ie>
2020-03-01 13:36:28 +0000

    Created add method and called it from main mathod

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 class Main {
-    // Your code here
+    
+    public static void main(String[] args) {
+        
+        //calling add method within print statement
+        System.out.println("8 + 14 = "+add(8,14));
+    }
+
+    //Adding the two integers together
+    public static int add(int x, int y){
+        return x+y;
+    }
 }
